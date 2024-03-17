ba166f6f4cefee7a835b80acdee8bd723cb6b90e
ekiz <kvmonrose@gmail.com>
2024-03-17 01:40:29 +0100

    gear slot count on CON instead of STR

--- system/src/documents/ActorSD.mjs
+++ system/src/documents/ActorSD.mjs
@@ -879,23 +879,15 @@ export default class ActorSD extends Actor {
 
 	numGearSlots() {
 		let gearSlots = shadowdark.defaults.GEAR_SLOTS;
-
 		if (this.type === "Player") {
-			const strength = this.system.abilities.str.base
-				+ this.system.abilities.str.bonus;
-
-			gearSlots = strength > gearSlots ? strength : gearSlots;
-
-			// Hauler's get to add their Con modifer (if positive)
-			const conModifier = this.abilityModifier("con");
-			gearSlots += this.system.bonuses.hauler && conModifier > 0
-				? conModifier
-				: 0;
-
+			const constitution = this.system.abilities.con.base + this.system.abilities.con.bonus;
+			gearSlots = constitution > gearSlots ? constitution : gearSlots;
+			// Hauler's get to add their Str modifer (if positive)
+			const strModifier = this.abilityModifierWithoutWounds("str");
+			gearSlots += this.system.bonuses.hauler && strModifier > 0 ? strModifier : 0;
 			// Add effects that modify gearslots
 			gearSlots += parseInt(this.system.bonuses.gearSlots, 10);
 		}
-
 		return gearSlots;
 	}
 
