c3150103d14ba3f5396ab9fc81a21e28a9fe814a
ekiz <kvmonrose@gmail.com>
2024-08-20 11:53:27 +0200

    fix invalid message data object

--- system/src/apps/CharacterGeneratorSD.mjs
+++ system/src/apps/CharacterGeneratorSD.mjs
@@ -367,7 +367,7 @@ export default class CharacterGeneratorSD extends FormApplication {
 			const message = `<h2><b>Charactacter Generator:</b></h2><h3>Rolled stats:</h3><p><b>STR</b>: ${this.formData.actor.system.abilities["str"].base}</p><p><b>DEX</b>: ${this.formData.actor.system.abilities["dex"].base}</p><p><b>CON</b>: ${this.formData.actor.system.abilities["con"].base}</p><p><b>INT</b>: ${this.formData.actor.system.abilities["int"].base}</p><p><b>WIS</b>: ${this.formData.actor.system.abilities["wis"].base}</p><p><b>CHA</b>: ${this.formData.actor.system.abilities["cha"].base}</p>`;
 			await ChatMessage.create({
 				user: game.user._id,
-				message,
+				content: message,
 				speaker: ChatMessage.getSpeaker(),
 				rollMode: CONST.DICE_ROLL_MODES.PUBLIC,
 			});
